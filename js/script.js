1403909fd453b863d297d2f96b6534deb7987dd6
KatarzynaZaloba <kzalobafrontend@gmail.com>
2023-01-27 23:08:45 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+console.log("Czesć");
+
+let przycisk = document.querySelector(".przycisk");
+let naglowek = document.querySelector(".naglowek");
+
+przycisk.addEventListener("click", () => {
+    naglowek.remove();
+});
+
+console.log(przycisk);
