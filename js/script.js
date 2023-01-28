a3033c6a21fbbe8c050b4ca2131d5ae8dbad9a49
KatarzynaZaloba <kzalobafrontend@gmail.com>
2023-01-28 08:44:12 +0100

    css, html and js change

--- js/script.js
+++ js/script.js
@@ -1,9 +1,33 @@
+let formElement = document.querySelector(".js-form");
+let amountElement = document.querySelector(".js-amount");
+let resultElement = document.querySelector(".js-result");
+let currencyRateElement = document.querySelector(".js-currency");
 
-let changeBackgroundButton = document.querySelector(".js-changeBackgroundButton");
-let body = document.body;
-let nextColorName = document.querySelector(".js-nextColorName");
 
-changeBackgroundButton.addEventListener("click", () => {
-    body.classList.toggle("body--grey");
-    nextColorName.innerText = body.classList.contains("body--grey") ? "białe" : "szare";
+let currencyEUR = 4.7076;
+let currencyUSD = 4.3252;
+let currencyGBP = 5.3505;
+
+
+formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let amount = amountElement.value;
+    let currency = currencyRateElement.value;
+    let result = resultElement.value;
+
+    switch (currency) {
+        case "EUR":
+            result = amount * currencyEUR;
+            break;
+        case "USD":
+            result = amount * currencyUSD;
+            break;
+        case "GBP":
+            result = amount * currencyGBP;
+            break;
+    }
+
+    resultElement.value = result.toFixed(2)
+
 });
